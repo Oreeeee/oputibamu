bc49d37ae9bfaec48648148e1e39bbdf7d145feb
Oreeeee <oree3ee@gmail.com>
2024-09-18 22:34:35 +0200

    Make replacements work correctly

--- ElektronikReplacements.go
+++ ElektronikReplacements.go
@@ -57,7 +57,7 @@ func (r *ReplacementsData) getCurrentLessonReplacements(day int, l Lesson, c Cla
 	}
 	for _, rep := range r.replacements {
 		lN, _ := strconv.Atoi(rep.LessonNumber)
-		if lN == l.number && rep.Classgroup[0] == c.nameShort {
+		if lN == l.number && rep.Classgroup[0] == c.nameShort && rep.Classgroup[1] == l.group.groupName {
 			return rep
 		}
 	}
